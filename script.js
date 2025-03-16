17a8d3db562ac6178e1bca832fed67bd6fed34ed
Roosevelt <roosevelt.2019206592@unicap.br>
2025-03-16 00:15:39 -0300

    Primeira versão do site

---
+++ script.js
@@ -0,0 +1,10 @@
+function comprar() {
+    alert("Redirecionando para a página de compras...");
+}
+
+function assinarNewsletter() {
+    let email = prompt("Digite seu e-mail para assinar nossa newsletter:");
+    if (email) {
+        alert("Obrigado por se inscrever!");
+    }
+}
